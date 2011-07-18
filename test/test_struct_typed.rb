9700a67248c502c4eb293e576a69d08d1925f95f
Matthew Willson <matthew@mediasp.com>
2011-07-18 19:14:15 +0100

    Change some Struct::Typed tests to use a named class

--- test/test_struct_typed.rb
+++ test/test_struct_typed.rb
@@ -2,28 +2,26 @@ require 'test/helpers'
 require 'thin_models/struct/typed'
 
 describe "ThinModels::Struct::Typed" do
-  def setup
-    @klass = Class.new(ThinModels::Struct::Typed) do
-      register_type do
-        property :foo, :integer
-        property :bar, :tuple, :integer, :boolean
-        property :baz, :boolean
-      end
+  class TestTypedClass < ThinModels::Struct::Typed
+    register_type do
+      property :foo, :integer
+      property :bar, :tuple, :integer, :boolean
+      property :baz, :boolean
     end
   end
 
   it 'should shallowly type-check non-lazy properties on construction' do
-    assert @klass.new(:foo => 123)
-    assert_raises(TypeError) {@klass.new(:foo => "abc")}
+    assert TestTypedClass.new(:foo => 123)
+    assert_raises(TypeError) {TestTypedClass.new(:foo => "abc")}
 
-    assert @klass.new(:foo => 123, :bar => [123, false])
-    assert @klass.new(:foo => 123, :bar => ["only","shallow"])
+    assert TestTypedClass.new(:foo => 123, :bar => [123, false])
+    assert TestTypedClass.new(:foo => 123, :bar => ["only","shallow"])
 
-    assert_raises(TypeError) {@klass.new(:foo => 123, :bar => "abc")}
+    assert_raises(TypeError) {TestTypedClass.new(:foo => 123, :bar => "abc")}
   end
 
   it "should shallowly type-check properties on attribute assignment" do
-    instance = @klass.new
+    instance = TestTypedClass.new
     instance.foo = 123
     instance.bar = [123, false]
     instance.bar = ["only", "shallow"]
@@ -33,21 +31,21 @@ describe "ThinModels::Struct::Typed" do
     assert_raises(TypeError) {instance.baz = "not a bool"}
   end
 
+  class TestTypedClassWithIdentity < ThinModels::Struct::Typed
+    register_type do
+      property :id, :integer
+      property :foo, :integer
+      property :bar, :tuple, :integer, :boolean
+      property :baz, :boolean
+    end
+    identity_attribute :id
+  end
+
   describe "in combo with Struct::IdentityMethods" do
     it "should work" do
-      @klass = Class.new(ThinModels::Struct::Typed) do
-        register_type do
-          property :id, :integer
-          property :foo, :integer
-          property :bar, :tuple, :integer, :boolean
-          property :baz, :boolean
-        end
-        identity_attribute :id
-      end
-
-      assert_equal @klass.new(:id => 123), @klass.new(:id => 123)
-      assert @klass.new(:id => 123).type_check_property(:id)
-      assert_raises(TypeError) {@klass.new.id = "not an int"}
+      assert_equal TestTypedClassWithIdentity.new(:id => 123), TestTypedClassWithIdentity.new(:id => 123)
+      assert TestTypedClassWithIdentity.new(:id => 123).type_check_property(:id)
+      assert_raises(TypeError) {TestTypedClassWithIdentity.new.id = "not an int"}
     end
   end
 
